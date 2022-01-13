cc68ae7fe063644848b4b7a38cd340c769972d7e
Akash Mandal <93242743+Akash6297@users.noreply.github.com>
2022-01-13 08:36:37 +0530

    edit contact.html & message.php
    edit contact.html & message.php

--- php/message.php
+++ php/message.php
@@ -22,4 +22,32 @@
   }else{
     echo "Email and message field is required!";
   }
+  
+  header("Location: contact.html");
+
 ?>
+
+<!-- <?php
+    $name =$_POST['name'];
+    $visitor_email = $_POST ['email'];
+    $message = $_POST['message'];
+
+
+    $email_from = '';
+
+    $email_subject = "New From Submission";
+
+    $email_body = "User Name: $name.\n".
+                    "User Email: $visitor_email.\n".
+                    "User Message: $message.\n";
+
+    $to =  'akashmandal6297@gmail.com';
+    
+    $headers ="From: $email_from \r\n";
+
+    $headers .= "Reply-To: $visitor_from \r\n";
+
+    mail($to,$email_subject,$email_body,$headers);
+
+    header("Location: contact.html");
+?> -->
